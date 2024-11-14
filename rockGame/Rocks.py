b0b1a8896c74eb35c7de198d7087cef0058dc35f
lilachHerzog <84073472+lilachHerzog@users.noreply.github.com>
2024-11-14 23:36:13 +0200

    Update Rocks.py
    added main() and fixed canWin()

--- rockGame/Rocks.py
+++ rockGame/Rocks.py
@@ -3,12 +3,13 @@ class Rocks:
     A class used to play a game where each player (out of 2) chooses a pile of rocks from one end of a row of rocks.
     The goal of the game is to reach the maximum number of stones.
     """
+ 
     def __init__(self, arr):
         self.rocks = {'p1': 0, 'p2': 0}
         self.arr = arr  
     
     def takePile(self, player ,side):
-       """adds rocks from the chosen pile to the chosen player """
+        """adds rocks from the chosen pile to the chosen player """
         self.rocks[player] += self.arr[side] # add selected rocks to the player's pile
         print(player, " takes ", self.arr.pop(side))
         return 
@@ -27,5 +28,22 @@ class Rocks:
 
     def canWin(self):
         """ The goal of the function is to determine whther the first player can win"""
-        self.findBestEdge('p1')
         return self.rocks['p1'] > self.rocks['p2']
+
+
+if __name__ == "__main__":
+    arrays = [[2, 5, 3, 2],
+              [1, 8, 3, 6, 4, 2],
+              [5, 9, 1, 4, 6, 8, 3],
+              [7, 4, 2, 5, 8, 1], 
+              [3, 8, 4, 1, 7, 5, 2],
+              [6, 1, 3, 5, 2],
+              [4, 2, 9, 3, 5, 6]] 
+    for arr in arrays:
+        print('\n', arr)
+        rocksAlg = Rocks(arr.copy())
+        rocksAlg.findBestEdge("p1")
+        if rocksAlg.canWin():
+            print ("YES! the first player won with", rocksAlg.rocks['p1'], " while the second player got only ", rocksAlg.rocks['p2'])  
+        else:
+            print ("noooo! the first player lost with", rocksAlg.rocks['p1'], " while the second player got ", rocksAlg.rocks['p2'])           
